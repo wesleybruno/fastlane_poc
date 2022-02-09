8b1157a66f7815ba90b84d5f8d6ba8e7d258be44
Wesley Oliveira <99283327+wesleyoliveira-ciandt@users.noreply.github.com>
2022-02-08 23:09:13 -0300

    feat: configure build types on ios, and adjust on android

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:test_fastlane/app/app.dart';
-import 'app/config/environment.dart';
 
 void main() {
-  Environment.prod('URL_PROD');
   runApp(const App());
 }
--- lib/main_development.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:test_fastlane/app/app.dart';
-import 'app/config/environment.dart';
-
-void main() {
-  Environment.prod('URL_DEV');
-  runApp(const App());
-}
